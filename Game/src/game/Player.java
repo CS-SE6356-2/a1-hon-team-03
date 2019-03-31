f576eba557a239ab6426aea86855a1ad7136b4fe
Shannen Barrameda <sbarrameda17@gmail.com>
2019-03-31 15:55:59 -0500

    created player, card, and deck classes. created new package called game in which the source files for these classes are contained. have not tested

---
+++ Game/src/game/Player.java
@@ -0,0 +1,34 @@
+package game;
+import java.util.*;
+
+public abstract class Player{
+    Hand myHand;
+    boolean dealer;
+    int score;
+    
+    public Player(){
+        myHand = new ArrayList<Hand>();
+        score = 0;
+    }
+    
+    abstract Card drawCard();
+    abstract void discardCard();
+    abstract void bid();
+    abstract void splitDeck(Deck deck);
+    abstract void giveCards(Player p);
+            
+    public int getScore(){
+        return score;
+    }
+    public Hand getHand(){
+        return myHand;
+    }
+    private void setDealer(boolean dealer){
+        this.dealer = dealer;
+    }
+    public boolean isDealer(){
+        return dealer;
+    }
+    
+    
+}

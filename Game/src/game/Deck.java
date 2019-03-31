c65bcbe5410b4ddffe08f7f871bc0f219da95ddb
Shannen Barrameda <sbarrameda17@gmail.com>
2019-03-31 18:40:21 -0500

    pushed deck/card/game classes, game class is empty

---
+++ Game/src/game/Deck.java
@@ -0,0 +1,47 @@
+//Shannen Barrameda sib170130 CS3354.HON Team 3
+package game;
+import java.util.*;
+
+public class Deck{
+    ArrayList<Card> deck;
+    int numCards = 0;
+    
+    public Deck(){
+       deck = new ArrayList<Card>();
+       for(int suit = 1; suit < 6; suit++){
+           for(int val = 1; val < 14; val++){
+                deck.add(new Card(val, suit));
+                numCards++;
+           }
+       }
+    }
+    
+    public void shuffle(){
+        Collections.shuffle(deck);
+    }
+    
+    public int cardsLeft(){
+        return numCards;
+    }
+    
+    public Card dealTopCard(){
+        return dealCard(0);
+    }
+    
+    public Card dealCard(int n){
+        if(cardsLeft() > 0){
+            numCards--;
+            return deck.remove(n);
+        }
+        else{
+            System.out.print("Deck is empty");
+            return null;
+        }
+    }
+    
+    public void printCards(){
+        for(Card c: deck){
+            System.out.println(c.toString());
+        }
+    }
+}

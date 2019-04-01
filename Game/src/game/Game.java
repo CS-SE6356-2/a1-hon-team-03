a3bf1294ca8b8e42044ecc13745a4fe44ba0e735
ifz160030 <italozev@gmail.com>
2019-04-01 04:03:15 -0500

    Update Game.java

--- Game/src/game/Game.java
+++ Game/src/game/Game.java
@@ -1,13 +1,29 @@
-
 package game;
 
-public class Game {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+import java.util.ArrayList;
+//game uses handler function which handles button presses etc.
+public abstract class Game extends EventHandler {
+    Deck starter;
+    Deck draw;
+    Deck discard;
+    ArrayList<View> views;
+    ArrayList<Player> players;
+    ArrayList<Integer> bidList;
+    String rule;
+    
+    public Game(){
+        players = new ArrayList<View>();
+        bidList = new ArrayList<View>();
+        starter = new Deck();
+        draw = new Deck();
+        discard = new Deck();
+        rule = "";
     }
     
+    abstract int calcScore(Player p)
+    abstract int evaluateScore()
+    
+    private ArrayList<View> getViews(){
+        return views;
+    }
 }

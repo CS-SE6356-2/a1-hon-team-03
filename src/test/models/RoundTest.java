fd33e5f9e0a882a89063c1cad2cc39e07df8dea3
Matthew Kunjummen <mkunjummen@gmail.com>
2019-04-09 23:09:11 -0500

    Test the Round class

---
+++ src/test/models/RoundTest.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import src.models.*;
+
+class RoundTest {
+    static int countLimit = 10;
+    static int viewableLimit = 10;
+    Round round;
+    int viewableCalls = 0;
+    int playerCalls = 0;
+
+    class PlayerStub extends Player implements Pile {
+        void pushCard(Card c) {}
+        Card drawCard() { return new Card(); }
+        void discardCard() {}
+        void bid() {}
+        void SplitDeck(Deck deck) {}
+        void giveCards(Player p) {}
+        Turn<int> takeTurn(List<Turn<Object>> hist) {
+            assertTrue(hist.size() <= viewableLimit);
+            return Turn<int>(this, this, 0);
+        }
+    }
+
+    @BeforeEach
+    void setup() {
+        round = new class extends Round<int> {
+            @Override
+            Player nextPlayer() {
+                playerCalls++;
+                if (playerCalls < countLimit) {
+                    return new PlayerStub();
+                } else {
+                    return null;
+                }
+            }
+
+            @Override
+            int numViewable() {
+                viewableCalls++;
+                return viewableLimit;
+            }
+        }
+    }
+
+    @Test
+    void testHitLimit() {
+        countLimit = 10;
+        viewableLimit = 20;
+        round.playRound();
+        assertEquals(countLimit, viewableCalls);
+        assertEquals(countLimit, playerCalls);
+    }
+
+    @Test
+    void testViewableLimit() {
+        countLimit = 20;
+        viewableLimit = 10;
+        round.playRound();
+    }
+
+}
